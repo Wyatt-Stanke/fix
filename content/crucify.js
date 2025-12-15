4af65d1d75e52e21dd84c7dffb9e8622e183a7cb
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2025-12-15 15:19:21 +0000

    OG sync

---
+++ content/crucify.js
@@ -0,0 +1,78 @@
+let centerL = screen.width * .5;
+let centerT = screen.height * .5;
+
+let i;
+let count = 0;
+let loop;
+
+function crucify(){
+  // scale
+//  console.log(scaleFactor);
+
+  
+  
+//  
+  
+  const CHARS_TO_SHOW = 80;
+   $.getJSON('bible.json', function(data) {
+    var bible = data;
+     i = randInt(1,31102);
+     
+     
+    loop = setInterval(function(){
+    history.pushState({}, '', `?=${i}`);
+      
+//    document.title = i;
+//    i++;
+    
+//    history.pushState({}, '', `?=${bible[i].verse + " — " + bible[i].text}`);
+    let verseText = bible[i].text;
+      
+//    if(verseText.length <= 0){
+//      console.log(bible[i].text.length);
+//      i--;
+//      if(i <= 0){ i = 31103; } // restart
+//      return;
+//    }
+      
+      
+//    let start =  Math.max(0, verseText.length - CHARS_TO_SHOW);
+//    let textToShow = verseText.slice(start);
+//      
+//      if(textToShow.length < 30){
+//        textToShow = bible[i].verse + ": " + textToShow;
+//      }
+
+//      if(charIndex == 0){
+//        textToShow = bible[i].verse + "——" + textToShow;
+//        document.title = bible[i].verse;
+//        history.pushState({}, '', `?=${i}`);
+//      }
+      
+      
+     document.title = `${bible[i].verse} — ${verseText}`;
+//     document.title = textToShow;
+//      history.pushState({}, '', `?=${count}`);
+      
+//      bible[i].text = verseText.substring(0, start);
+//      console.log(textToShow, "———",  bible[i].text);
+//      count++;
+      
+      i--;
+      
+      if(i <= 0){ i = 31103; } // restart
+      
+  }, 500);
+     
+     
+  });
+
+  
+}
+
+
+
+
+function randInt(min, max) { 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
